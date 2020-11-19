cae22b438fd8151454c40b0ca79419e220b75e08
江国庆 <guoqingjiang@deepglint.com>
2020-11-19 16:08:41 +0800

    improve test and README

--- README.md
+++ README.md
@@ -1,7 +1,67 @@
-GoMark is a tool for:
-1. latency statistics
-2. counter
-3. qps statistics
-4. ...
+# Introduce
+GoMark is a tool for system status monitoring and it implements some variables exactly like bvar in brpc, but using go instead of c++.
 
-It should be simple to use, and it is expected with high performance.
+For more detailed information, see:
+https://github.com/apache/incubator-brpc/blob/master/docs/en/bvar.md
+
+
+
+# Usage
+## Init
+gomark monitor variable statistics through HTTP web server, so you need call
+```go
+StartHTTPServer(port int)
+```
+to start HTTP service before using.
+
+## Variable Create
+A variable is an entity that maintains all information of statistics. There are several variables, and can be created by:
+```
+NewLatencyRecorder
+NewAdder
+NewCounter
+NewQPS
+NewMaxer
+NewWindowMaxer
+```
+
+Each of above will create a variable and returns as `gmi.Marker`, which is an interface:
+```go
+// Marker is an interface to provide variable marking.
+type Marker interface {
+	// Mark a number, the number definition is bound to marker itself.
+	Mark(n int32)
+	// Stop this marking.
+	Cancel()
+}
+```
+
+Call `Mark` to send a marking point to variable and `Cancel` to stop using(and never use it).
+
+# Monitor
+
+Visit http://ip:port/vars to monitor system statistics.
+
+# Performance
+Test method: in one goroutine, continuouesly marking for 10 million marks, get the time elasped and
+calculate how many marks can be done in one second(QPS).
+
+
+updated 2020.11.19
+
+| Variable        | QPS     |
+| --              | --      |
+| MaxWindow       | 6214338 |
+| Maxer           | 6497730 |
+| Adder           | 6445707 |
+| LatencyRecorder | 5411037 |
+| QPS             | 6837910 |
+| Counter         | 6163937 |
+
+
+# Test
+`cmd/main/gomark.go` is used run tests. It use glog for logging, so add ` -stderrthreshold=INFO` in command line:
+```
+go run gomark.go -stderrthreshold=INFO 
+```
+Read the usage and run test.
--- cmd/main/gomark.go
+++ cmd/main/gomark.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bufio"
 	"flag"
+	"fmt"
 	"math"
 	"math/rand"
 	"os"
@@ -15,150 +16,195 @@ import (
 	"github.com/golang/glog"
 )
 
+var mode = ""
+
+func wait(test string) {
+	if mode != "step" {
+		return
+	}
+	glog.Info("Press 'Enter' to continue ", test)
+	_, _ = bufio.NewReader(os.Stdin).ReadBytes('\n')
+}
+func pause() {
+	if mode != "perf" {
+		time.Sleep(time.Duration(rand.Intn(28)*3+17) * time.Millisecond)
+	}
+}
+
+func reportPerf(name string, start time.Time, total int) {
+	if mode == "perf" {
+		du := time.Since(start).Seconds()
+		qps := int(float64(total) / du)
+		glog.Infof("%s qps: %d", name, qps)
+	}
+}
+
 func testMaxWindow(name string, total int) {
-	glog.Info("Maxer window")
+	wait(name)
 	ad := gomark.NewWindowMaxer(name)
+	start := time.Now()
 	for i := 0; i < total; i++ {
 		v := rand.Int31n(10) + 1
 		ad.Mark(v)
-		//glog.Infof("mark %d", v)
-		time.Sleep(100 * time.Millisecond)
+		pause()
 	}
+	reportPerf(name, start, total)
 	ad.Cancel()
 	gm.MakeSureEmpty()
 }
 func testMaxer(name string, total int) {
-	glog.Info("Maxer")
+	wait(name)
 	ad := gomark.NewMaxer(name)
+	start := time.Now()
 	for i := 0; i < total; i++ {
 		v := rand.Int31n(10) + 1
 		ad.Mark(v)
 		//glog.Infof("mark %d", v)
-		time.Sleep(100 * time.Millisecond)
+		pause()
 	}
+	reportPerf(name, start, total)
 	ad.Cancel()
 	gm.MakeSureEmpty()
 }
 func testAdder(name string, total int) {
-	glog.Info("Adder")
+	wait(name)
 	ad := gomark.NewAdder(name)
+	start := time.Now()
 	for i := 0; i < total; i++ {
 		v := rand.Int31n(11) - 5
 		ad.Mark(v)
 		//glog.Infof("mark %d", v)
-		time.Sleep(100 * time.Millisecond)
+		pause()
 	}
+	reportPerf(name, start, total)
 	ad.Cancel()
 	gm.MakeSureEmpty()
 }
 func testCounter(name string, total int) {
-	glog.Info("Counter")
+	wait(name)
 	cnt := gomark.NewCounter(name)
+	start := time.Now()
 	for i := 0; i < total; i++ {
 		v := rand.Int31n(10) + 1
 		cnt.Mark(v)
 		//glog.Infof("mark %d", v)
-		time.Sleep(100 * time.Millisecond)
+		pause()
 	}
+	reportPerf(name, start, total)
 	cnt.Cancel()
 	gm.MakeSureEmpty()
 }
 func testQPS(name string, total int) {
-	glog.Info("QPS")
+	wait(name)
 
 	qps := gomark.NewQPS(name)
+	start := time.Now()
 	for i := 0; i < total; i++ {
 		//v := rand.Int31n(10) + 1
 		qps.Mark(6)
 		//glog.Infof("mark %d", v)
 		//time.Sleep(time.Duration(rand.Intn(28)*3+17) * time.Millisecond)
-		time.Sleep(100 * time.Millisecond)
+		pause()
 	}
+	reportPerf(name, start, total)
 	qps.Cancel()
 	gm.MakeSureEmpty()
 }
 func testLatencyRecorder(name string, total int) {
-	glog.Info("Latency Recorder")
+	wait(name)
 
 	lr := gomark.NewLatencyRecorder(name)
+	start := time.Now()
 	for i := 0; i < total; i++ {
 		v := rand.Int31n(100) + 1
 		//lr.Mark(70)
 		lr.Mark(v)
 		//glog.Infof("mark %d", v)
-		time.Sleep(time.Duration(rand.Intn(28)*3+17) * time.Millisecond)
+		pause()
 	}
 
+	reportPerf(name, start, total)
 	lr.Cancel()
 	gm.MakeSureEmpty()
 }
 func testPecentile(total int) {
-	glog.Info("Percentile")
+	wait("percentile")
 
 	m := gomark.NewPercentile()
 	for i := 0; i < total; i++ {
 		v := rand.Int31n(100) + 1
 		m.Push(gm.Mark(v))
 		//glog.Infof("mark %d", v)
-		time.Sleep(time.Duration(rand.Intn(28)*3+17) * time.Millisecond)
+		pause()
 	}
 
 	m.Dispose()
 	gm.MakeSureEmpty()
 }
 
-var stop = false
-
-func wait(test string) {
-	if !stop {
-		return
-	}
-	glog.Info("Press 'Enter' to continue ", test)
-	_, _ = bufio.NewReader(os.Stdin).ReadBytes('\n')
+func usage() {
+	str := `
+gomark -p <port> -m <mode> -n <number>
+<port>: http server port
+<mode>: test mode
+        - walk: test one by one, each runs <number> marks, and sleep a litter while after each mark.
+        - step: just like walk, but it pauses on starting of each test
+        - perf: runs <number> marks and do not sleep after mark. Then calculate QPS of this variable.
+        - forever: start <number> goroutines and each runs all supported variables forever.
+`
+	fmt.Println(str)
 }
 func main() {
 	total := 0
 	port := 0
-	flag.BoolVar(&stop, "s", false, "set to true to test step by step")
-	flag.IntVar(&total, "n", math.MaxInt64-1, "how many time for each var to run, not present for infinite")
+	flag.StringVar(&mode, "m", "", "test mode: walk,perf,step,forever")
+	flag.IntVar(&total, "n", 100, "how many time for each var to run, or how many batches in forever mode")
 	flag.IntVar(&port, "p", 7770, "http port, default 7770")
 	flag.Parse()
+
 	gomark.StartHTTPServer(port)
 
-	if total == 0 {
-		total = math.MaxInt64 - 1
+	switch mode {
+	case "walk":
+	case "step":
+	case "perf":
+	case "forever":
+		glog.Info("")
 		wg := sync.WaitGroup{}
 		wg.Add(1)
 		gm.EnableInternalVariables()
 
-		for i := 0; i < 1000; i++ {
-			go testMaxWindow("max_win_"+strconv.Itoa(i), total)
-			go testMaxer("max_"+strconv.Itoa(i), total)
-			go testAdder("adder_"+strconv.Itoa(i), total)
-			go testLatencyRecorder("latency_recorder_"+strconv.Itoa(i), total)
-			go testQPS("qps_"+strconv.Itoa(i), total)
-			go testCounter("counter_"+strconv.Itoa(i), total)
+		for i := 0; i < total; i++ {
+			go testMaxWindow("max_win_"+strconv.Itoa(i), math.MaxInt64)
+			go testMaxer("max_"+strconv.Itoa(i), math.MaxInt64)
+			go testAdder("adder_"+strconv.Itoa(i), math.MaxInt64)
+			go testLatencyRecorder("latency_recorder_"+strconv.Itoa(i), math.MaxInt64)
+			go testQPS("qps_"+strconv.Itoa(i), math.MaxInt64)
+			go testCounter("counter_"+strconv.Itoa(i), math.MaxInt64)
 		}
 
 		go testPecentile(total)
 		wg.Wait()
-	} else {
-		wait("testMaxWindow")
-		testMaxWindow("testMaxWindow", total)
-		wait("testMaxer")
-		testMaxer("testMaxer", total)
-		wait("testAdder")
-		testAdder("testAdder", total)
-		wait("testLatencyRecorder")
-		testLatencyRecorder("testLatencyRecorder", total)
-		wait("testQPS")
-		testQPS("testQPS", total)
-		wait("testCounter")
-		testCounter("testCounter", total)
-		wait("testPercentile")
-		testPecentile(total)
-		glog.Info("exit")
-
-		gm.MakeSureEmpty()
+	default:
+		glog.Info("unknown mode: ", mode)
+		usage()
+		os.Exit(1)
 	}
+
+	if total == 0 {
+		total = 100
+	}
+
+	glog.Infof("mode: %s total %d", mode, total)
+
+	testMaxWindow("testMaxWindow", total)
+	testMaxer("testMaxer", total)
+	testAdder("testAdder", total)
+	testLatencyRecorder("testLatencyRecorder", total)
+	testQPS("testQPS", total)
+	testCounter("testCounter", total)
+	testPecentile(total)
+	gm.MakeSureEmpty()
+
+	glog.Info("exit")
 }
