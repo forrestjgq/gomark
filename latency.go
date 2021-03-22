de258a835327543fd6ef2b30d2e1ad46bade5d79
江国庆 <mr.jianggq@163.com>
2021-03-22 16:16:00 +0800

    add Latency() for Latency

--- latency.go
+++ latency.go
@@ -13,28 +13,30 @@ import (
 type Latency struct {
 	marker gmi.Marker
 	start  time.Time
+	latency int32
 }
 
 // Mark calculate how many ms has passed since creation of this Latency
 // and mark it.
 func (l *Latency) Mark() {
-	if l.marker == nil {
-		glog.Warningf("latency is cancelled")
-		return
-	}
-
 	du := time.Since(l.start).Milliseconds()
 	if du > math.MaxInt32 {
 		glog.Warningf("latency overflow: %d ms", du)
 		du = math.MaxInt32
 	}
-	l.marker.Mark(int32(du))
+	l.latency = int32(du)
+	if l.marker != nil {
+		l.marker.Mark(l.latency)
+	}
 }
 
 // Cancel will clear this latency and it will not be able to use any more.
 func (l *Latency) Cancel() {
 	l.start = time.Time{}
 	l.marker = nil
+}
+func (l *Latency) Latency() int32 {
+	return l.latency
 }
 
 // NewLatency create a latency for a Latency Recorder created by NewLatencyRecorder.
